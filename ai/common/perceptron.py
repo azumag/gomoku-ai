c52e5048fe9a810fcc93018fe0c0b54020b6cf14
azumag <tsubasa.azumagakito@gmail.com>
2017-01-20 16:09:52 +0900

    update

--- ai/common/perceptron.py
+++ ai/common/perceptron.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import util as u
 
 class Perceptron:
-    data_path = '/home/azumagakito/gomoku-ai/ai/tmp/'
-    layer = 3
+    layer = 0
     weights = []
     neurons = []
     biases  = []
+    x  = None
     y_ = None
     util = None
     sess = None
@@ -16,17 +16,9 @@ class Perceptron:
         self.sess = tf.Session()
         self.layer = layer
 
-    def weight_variable(self,shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial)
-
-    def bias_variable(self,shape):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial)
-
-    def train(self, file_name, batch):
-        train_data  = self.util.load_data(self.data_path+'h-'+file_name)
-        train_label = self.util.load_data(self.data_path+'a-'+file_name)
+    def train(self, train_file, label_file, batch):
+        train_data  = self.util.load_data(train_file)
+        train_label = self.util.load_data(label_file)
         train_step = self.layer_set()
 
         self.util.print_start()
@@ -37,25 +29,26 @@ class Perceptron:
             print 'batch: ' + str(i) + ' - ' + str(batch_range)
             batch_data  = train_data[i: batch_range]
             batch_label = train_label[i: batch_range]
-            self.sess.run(train_step, feed_dict={x: batch_data, y_: batch_label})
+            self.sess.run(train_step, feed_dict={self.x: batch_data, self.y_: batch_label})
         print "--- end train ---"
         self.util.print_end()
 
-    def test(self, file_name):
-        test_data  = self.util.load_data(data_path+'h-'+file_name)
-        test_label = self.util.load_data(data_path+'a-'+file_name)
+    def test(self, data_file, label_file):
+        test_data  = self.util.load_data(data_file)
+        test_label = self.util.load_data(label_file)
 
-        correct_prediction = tf.equal(tf.argmax(self.neurons[-1],1), tf.argmax(y_,1))
+        correct_prediction = tf.equal(tf.argmax(self.neurons[-1],1), tf.argmax(self.y_,1))
         accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
-        print(self.sess.run(accuracy, feed_dict={x: test_data, y_: test_label}))
+        return (self.sess.run(accuracy, feed_dict={self.x: test_data, self.y_: test_label}))
 
     def layer_set(self):
-        x = tf.placeholder(tf.float32, [None, 81])
+        self.x = tf.placeholder(tf.float32, [None, 81])
 
-        self.weights = [ self.weight_variable(tf.zeros([81, 81])) for i in range(self.layer)]
-        self.biases  = [ self.bias_variable(tf.zeros([81, 81])) for i in range(self.layer)]
-        self.neurons = [ tf.nn.relu(tf.matmul(x, weights[0])+b[0]) ]
+        self.weights = [ tf.Variable(tf.zeros([81, 81])) for i in range(self.layer)]
+        self.biases  = [ tf.Variable(tf.zeros([81])) for i in range(self.layer)]
+        self.neurons = [ tf.nn.relu(tf.matmul(self.x, self.weights[0])+self.biases[0]) ]
+
         for i in range(1, self.layer-1):
             self.neurons.append(
                 tf.nn.relu(
@@ -70,7 +63,7 @@ class Perceptron:
         cross_entropy = -tf.reduce_sum(self.y_*tf.log(self.neurons[-1]))
         train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
-        init = tf.global_variables_initializer()
-        sess.run(init)
+        init = tf.initialize_all_variables()
+        self.sess.run(init)
 
         return train_step
--- ai/common/train.py
+++ ai/common/train.py
@@ -3,6 +3,13 @@
 import tensorflow as tf
 import perceptron as nn
 
-n = nn.Perceptron(3)
-n.train('4v4',1000)
+accuracies = []
+
+#for i in range(1, 10):
+n = nn.Perceptron(4)
+n.train('../tmp/h-4v4','../tmp/a-4v4', 1)
+result = n.test('../tmp/h-4v4','../tmp/a-4v4')
+accuracies.append(result)
+
+print accuracies
 
