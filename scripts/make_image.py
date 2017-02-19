96a944e1e9b7c82d3a2b885455852015406b0fec
azumag <tsubasa.azumagakito@gmail.com>
2017-02-19 19:30:05 +0900

    add tests

--- scripts/make_image.py
+++ scripts/make_image.py
@@ -1,14 +1,26 @@
-
 from PIL import Image
 import numpy
 import os
+import sys
 
-imagesize = (81, 81)
+imagesize = (9, 9)
+
+name = sys.argv[1]
+input_d = sys.argv[2:]
+
 
 im2 = Image.new('RGBA', imagesize)
 
 for x in range(imagesize[0]):
     for y in range(imagesize[1]):
-        im2.putpixel((x,y),(100,255,1,0))
+        cell = input_d[x*imagesize[0]+y]
+        if cell == '0':
+            im2.putpixel((x,y),(255,0,0,0))
+        elif cell == '1':
+            im2.putpixel((x,y),(0,255,0,0))
+        elif cell == '2':
+            im2.putpixel((x,y),(0,0,255,0))
+        else:
+            print 'unknown errr'
 
-im2.save('noize.jpg')
+im2.save(name+'.bmp')
